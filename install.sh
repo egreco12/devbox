53a4a4ccc0f46fd8998232645187f91f768a532e
Evan Greco <e.greco12@gmail.com>
2017-04-26 12:55:17 -0700

    clear initial values from fork

--- install.sh
+++ install.sh
@@ -1,14 +1,4 @@
-echo "Installing some stuff..."
-sudo apt-get update && sudo apt-get install -y build-essential emacs zsh git curl
-chsh -s /bin/zsh
+echo "Installing essentials from apt-get"
+sudo apt-get update && sudo apt-get install -y build-essential git curl
 
-# Prezto
-git clone --recursive https://github.com/sorin-ionescu/prezto.git ~/.zprezto
-for rcfile in ~/.zprezto/runcoms/*; do ln -s "$rcfile" "~/.${rcfile}"; done; rm ~/.README.md
-
-# Emacs Prelude
-curl -L http://git.io/epre | sh
-
-echo "(setq mode-require-final-newline nil)" >> ~/.emacs.d/personal/whitespace.el
-echo "(add-hook 'before-save-hook 'delete-trailing-whitespace)" >> ~/.emacs.d/personal/whitespace.el
 echo "Done"
